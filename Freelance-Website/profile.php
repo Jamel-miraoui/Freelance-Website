7b415befb4c3e72f83124d4581eb118420576f91
Miraoui Jamel <jamel.miraoui.sl@gmail.com>
2023-06-23 01:11:06 +0100

    last one tonight
    bhy jamel hadha bach tetdhaket cha3malet enti bahy 9a3d traka7 fel page bta3 job details w t7b tbadel el theme el website b kolou ayya deman taw nal9aw 7al bahy

--- Freelance-Website/profile.php
+++ Freelance-Website/profile.php
@@ -58,6 +58,26 @@ error_reporting(E_ALL);
     <link rel="stylesheet" href="profile.css">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
 	<link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="icon" href="images/favicon.png" type="image/x-icon">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/scrollbar.css">
+	<link rel="stylesheet" href="css/fontawesome/fontawesome-all.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/jquery-ui.css">
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/tipso.css">
+	<link rel="stylesheet" href="css/chosen.css">
+	<link rel="stylesheet" href="css/prettyPhoto.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/color.css">
+	<link rel="stylesheet" href="css/transitions.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	<script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 </head>
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
